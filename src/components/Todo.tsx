d9cdcedf9d2893061191e9e60b3c4a7991c08dca
Aditya Kumar Gupta <adityakgupta206@gmail.com>
2026-07-08 16:06:05 +0545

    feat: enhance Todo component with task editing, deletion, and sorting functionality

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -6,54 +6,123 @@ import { Badge } from "./ui/badge";
 import { BiSolidEditAlt } from "react-icons/bi";
 import { AiFillDelete } from "react-icons/ai";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "./ui/select";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter,
+} from "./ui/dialog";
 import type { priorityType, task } from "@/types";
-import { setInLocalStorage, getFromLocalStorage } from "@/lib/functions";
+import {
+  setInLocalStorage,
+  getFromLocalStorage,
+  sortTasks,
+} from "@/lib/functions";
 
 const Todo = () => {
   const [taskValue, setTaskValue] = useState<string>("");
-  const [priorityValue, setPriorityValue] = useState<"High" | "Medium" | "Low">("Medium");
+  const [updatedTaskValue, setUpdatedTaskValue] = useState<string>("");
+  const [currentTaskValue, setCurrentTaskValue] = useState<string>("");
+  const [priorityValue, setPriorityValue] = useState<"High" | "Medium" | "Low">(
+    "Medium",
+  );
+  const [updatedPriorityValue, setUpdatedPriorityValue] = useState<"High" | "Medium" | "Low">("Medium");
+  const [currentPriorityValue, setCurrentPriorityValue] = useState< "High" | "Medium" | "Low">("Medium");
   const [allTasks, setAllTasks] = useState<task[]>([]);
 
   const handleSubmit = (event: React.SubmitEvent<HTMLFormElement>) => {
     event.preventDefault();
     const id: string = crypto.randomUUID();
     const newTask: task = {
-        id: id,
-        isCompleted: false,
-        task: taskValue,
-        priority: priorityValue
-    }
+      id: id,
+      isCompleted: false,
+      task: taskValue,
+      priority: priorityValue,
+    };
 
     setAllTasks((prevTask): task[] => {
-        const updatedTasks: task[] = [...prevTask, newTask];
-        console.log(updatedTasks);
-        setInLocalStorage<task[]>("todos", updatedTasks);
+      const updatedTasks: task[] = [...prevTask, newTask];
+      console.log(updatedTasks);
+      setInLocalStorage<task[]>("todos", updatedTasks);
 
-        return updatedTasks;
-    })
+      return updatedTasks;
+    });
 
     setTaskValue("");
     setPriorityValue("Medium");
   };
 
+  const handleCheck = (id: string, checked: boolean) => {
+    const updatedTask: task[] = allTasks.map((task: task): task => {
+      if (task.id === id) {
+        return {
+          ...task,
+          isCompleted: checked,
+        };
+      } else {
+        return task;
+      }
+    });
+
+    setAllTasks(updatedTask);
+  };
+
+  const handleDelete = (id: string) => {
+    const updatedTask: task[] = allTasks.filter(
+      (task: task): boolean => task.id !== id,
+    );
+
+    console.log(updatedTask);
+    setAllTasks(updatedTask);
+  };
+
+  const handleModalOpens = (taskName: string, priority: priorityType) => {
+    setCurrentTaskValue(taskName);
+    setCurrentPriorityValue(priority);
+  }
+
+  const handleEditTask = (id: string) => {
+    const updatedTasks = allTasks.map((task: task): task => {
+      if(task.id === id){
+        return {
+          ...task,
+          task: updatedTaskValue || currentTaskValue,
+          priority: updatedPriorityValue || currentPriorityValue
+        }
+      } else {
+        return task;
+      }
+    })
+
+    setAllTasks(updatedTasks);
+  }
+
+  // retrieve the data from localstorage whenever the page loads for first time
   useEffect(() => {
     const data = getFromLocalStorage<task[]>("todos");
 
-    if(data){
-        setAllTasks(data);
-        console.log(data);
+    if (data) {
+      const newData = sortTasks(data);
+      setAllTasks(data);
+      console.log(newData);
     } else {
-        console.log("Error");
+      console.log("Error");
     }
-  
-  }, [])
-  
+  }, []);
+
+  // change the value inside localstorage whenever the allTask and setAllTask is Manipulated
+  useEffect(() => {
+    setInLocalStorage("todos", allTasks);
+  }, [allTasks, setAllTasks]);
 
   return (
     <div className="main-component flex flex-col justify-center items-center gap-2 h-full min-w-2xl">
@@ -74,17 +143,19 @@ const Todo = () => {
             setTaskValue(event.target.value)
           }
         />
-        <Select onValueChange={(value) => setPriorityValue(value as priorityType)}>
-            <SelectTrigger className="w-1/5">
-                <SelectValue placeholder="Priority" />
-            </SelectTrigger>
-            <SelectContent>
-                <SelectItem value="High">High</SelectItem>
-                <SelectItem value="Medium">Medium</SelectItem>
-                <SelectItem value="Low">Low</SelectItem>
-            </SelectContent>
+        <Select
+          onValueChange={(value) => setPriorityValue(value as priorityType)}
+        >
+          <SelectTrigger className="w-1/5">
+            <SelectValue placeholder="Priority" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="High">High</SelectItem>
+            <SelectItem value="Medium">Medium</SelectItem>
+            <SelectItem value="Low">Low</SelectItem>
+          </SelectContent>
         </Select>
-        
+
         <Button type="submit">Add Task</Button>
       </form>
 
@@ -95,23 +166,138 @@ const Todo = () => {
           <div className="w-1/8 text-center">Priority</div>
           <div className="w-1/5 text-center">Actions</div>
         </div>
-        
+
         <div className="tasks flex flex-col gap-1 overflow-y-auto">
-          <div className="task w-full flex gap-0.5 bg-yellow-100 h-16 ">
-            <div className="w-1/8 text-center flex justify-center items-center">
-              <Checkbox />
-            </div>
-            <div className="w-11/20 text-center flex items-center justify-center">Task</div>
-            <div className="w-1/8 text-center flex items-center justify-center">
-              <Badge className="bg-green-200 text-green-700 dark:bg-green-950 dark:text-green-300">
-                Green
-              </Badge>
-            </div>
-            <div className="w-1/5 text-center flex justify-center gap-8 items-center">
-                <div id="edit"><BiSolidEditAlt className="hover"/></div>
-                <div id="delete"><AiFillDelete className="hover"/></div>
-            </div>
-          </div>
+          {allTasks &&
+            allTasks.map((task: task) => (
+              <div
+                className="task w-full flex gap-0.5 bg-yellow-100 h-16 "
+                key={task.id}
+              >
+                <div className="w-1/8 text-center flex justify-center items-center">
+                  <Checkbox
+                    checked={task.isCompleted}
+                    onCheckedChange={(checked) => handleCheck(task.id, checked)}
+                  />
+                </div>
+                <div className="w-11/20 text-center flex items-center justify-center">
+                  {task.task}
+                </div>
+                <div className="w-1/8 text-center flex items-center justify-center">
+                  <Badge className="bg-green-200 text-green-700 dark:bg-green-950 dark:text-green-300">
+                    {task.priority}
+                  </Badge>
+                </div>
+                <div className="w-1/5 text-center flex justify-center gap-8 items-center">
+                  {/* <div id="edit"><BiSolidEditAlt className="hover"/></div> */}
+
+                  <Dialog onOpenChange={() => handleModalOpens(task.task, task.priority)}>
+                    <DialogTrigger>
+                      <BiSolidEditAlt className="cursor-pointer" />
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Edit Task</DialogTitle>
+                        <DialogDescription>
+                          Update task details below.
+                        </DialogDescription>
+                      </DialogHeader>
+
+                      {/* Old task name (read-only) */}
+                      <div className="space-y-2">
+                        <label className="text-sm font-medium">
+                          Old Task Name
+                        </label>
+                        <Input value={task.task} disabled />
+                      </div>
+
+                      {/* New task name (editable) */}
+                      <div className="space-y-2">
+                        <label className="text-sm font-medium">
+                          New Task Name
+                        </label>
+                        <Input
+                          placeholder={currentTaskValue}
+                          value={updatedTaskValue}
+                          defaultValue={task.task}
+                          // onChange={(e) => {
+                          //   const updated = allTasks.map((t) =>
+                          //     t.id === task.id
+                          //       ? { ...t, task: e.target.value }
+                          //       : t,
+                          //   );
+                          //   setAllTasks(updated);
+                          // }}
+                          onChange={(event: React.ChangeEvent<HTMLInputElement>) => setUpdatedTaskValue(event.target.value)}
+
+                        />
+                      </div>
+
+                      {/* Old priority (read-only) */}
+                      <div className="space-y-2">
+                        <label className="text-sm font-medium">
+                          Old Priority
+                        </label>
+                        <Select value={task.priority} disabled>
+                          <SelectTrigger>
+                            <SelectValue />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="High">High</SelectItem>
+                            <SelectItem value="Medium">Medium</SelectItem>
+                            <SelectItem value="Low">Low</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </div>
+
+                      {/* New priority (editable) */}
+                      <div className="space-y-2">
+                        <label className="text-sm font-medium">
+                          New Priority
+                        </label>
+                        <Select
+                          defaultValue={currentPriorityValue}
+                          // onValueChange={(val) => {
+                          //   const updated = allTasks.map((t) =>
+                          //     t.id === task.id
+                          //       ? { ...t, priority: val as priorityType }
+                          //       : t,
+                          //   );
+                          //   setAllTasks(updated);
+                          // }}
+
+                          onValueChange={(value) => setUpdatedPriorityValue(value as priorityType)}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select priority" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value="High">High</SelectItem>
+                            <SelectItem value="Medium">Medium</SelectItem>
+                            <SelectItem value="Low">Low</SelectItem>
+                          </SelectContent>
+                        </Select>
+                      </div>
+
+                      <DialogFooter>
+                        <Button
+                          onClick={() => handleEditTask(task.id)}
+                        >
+                          Save Changes
+                        </Button>
+                      </DialogFooter>
+                    </DialogContent>
+                  </Dialog>
+
+                  <div id="delete" className="bg-red">
+                    <AiFillDelete
+                      className="hover"
+                      onClick={() => handleDelete(task.id)}
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
         </div>
       </div>
     </div>
--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,3 +1,5 @@
+import type { task } from "@/types";
+
 export const setInLocalStorage = <T>(key: string,item: T) => {
     localStorage.setItem(key, JSON.stringify(item));
 }
@@ -9,4 +11,17 @@ export const getFromLocalStorage = <T>(key: string): T | null => {
     }
 
     return null;
+}
+
+export const sortTasks = (taskArray: task[]): task[] => {
+    let newArray: task[];
+    const priorityOrder: {High: number; Medium: number; Low: number} = {
+        High: 3,
+        Medium: 2,
+        Low: 1
+    }
+
+    newArray = taskArray.sort((a, b) => priorityOrder[b.priority] - priorityOrder[a.priority]);
+
+    return newArray;
 }
